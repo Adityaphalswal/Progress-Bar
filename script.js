c934377b5e325d0fb5a228f194ef716e9d233318
Aditya Phalswal <47073054+Adityaphalswal@users.noreply.github.com>
2021-09-21 09:19:35 +0530

    1.1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
+// console.log(circles)
 
 let currentActive = 1
 
@@ -10,12 +11,14 @@ next.addEventListener('click', () => {
     if (currentActive > circles.length) {
         currentActive = circles.length
     }
+    update()
 })
 
 prev.addEventListener('click', () => {
     currentActive--;
     if (currentActive < 1) {
-        currentActive = 1}
+        currentActive = 1
+    }
         update() 
     
 })
@@ -23,10 +26,21 @@ prev.addEventListener('click', () => {
 function update() {
     circles.forEach((circle, idx) => {
         if(idx < currentActive) {
-            circles.classList.add('active')
+            circle.classList.add('active')
         } else {
-            circles.classList.remove('active')
+            circle.classList.remove('active')
         }
-    });
+    })
+
     const actives = document.querySelectorAll('.active')
+    progress.style.width = (actives/length-1) /(circles.length)  *100 +'%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
 }
